dd8db8460bd07aac5e5bc3f81f5ec0c1582637b7
Rey Victor Mendillo <59197211+rvmendillo@users.noreply.github.com>
2026-09-03 20:23:40 +0800

    Add MaiChartMaker/Sources/DirectoryPicker.swift

---
+++ MaiChartMaker/Sources/DirectoryPicker.swift
@@ -0,0 +1,33 @@
+import SwiftUI
+import UniformTypeIdentifiers
+import UIKit
+
+struct DirectoryPicker: UIViewControllerRepresentable {
+    let onPick: (URL) -> Void
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(onPick: onPick)
+    }
+
+    func makeUIViewController(context: Context) -> UIDocumentPickerViewController {
+        let picker = UIDocumentPickerViewController(forOpeningContentTypes: [.folder], asCopy: false)
+        picker.allowsMultipleSelection = false
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIDocumentPickerViewController, context: Context) {}
+
+    final class Coordinator: NSObject, UIDocumentPickerDelegate {
+        let onPick: (URL) -> Void
+
+        init(onPick: @escaping (URL) -> Void) {
+            self.onPick = onPick
+        }
+
+        func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+            guard let url = urls.first else { return }
+            onPick(url)
+        }
+    }
+}
