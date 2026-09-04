c1ffb318db96df7e0263f0404958d8295dbe01b0
Rey Victor Mendillo <59197211+rvmendillo@users.noreply.github.com>
2026-09-04 13:35:46 +0800

    Add Aether Rift app entry point

---
+++ AetherRift/Sources/AetherRiftApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct AetherRiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
