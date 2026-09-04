bbadc038bddfd789720d65d2d9a767e12684c545
Rey Victor Mendillo <59197211+rvmendillo@users.noreply.github.com>
2026-09-04 12:00:49 +0800

    Add branded RuneTamer launch splash

--- RuneTamerGame/RuneTamer/RootView.swift
+++ RuneTamerGame/RuneTamer/RootView.swift
@@ -2,12 +2,28 @@ import SwiftUI
 
 struct RootView: View {
     @EnvironmentObject var game: GameState
+    @State private var showSplash = true
 
     var body: some View {
-        switch game.screen {
-        case .world: WorldView()
-        case .collection: CollectionView()
-        case .quests: QuestView()
+        ZStack {
+            switch game.screen {
+            case .world: WorldView()
+            case .collection: CollectionView()
+            case .quests: QuestView()
+            }
+
+            if showSplash {
+                RuneTamerSplash()
+                    .transition(.opacity.combined(with: .scale(scale: 1.03)))
+                    .zIndex(10)
+            }
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.35) {
+                withAnimation(.easeInOut(duration: 0.55)) {
+                    showSplash = false
+                }
+            }
         }
     }
 }
