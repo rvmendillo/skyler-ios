ac1cfa8fcd452757c7311c78e891f6776f2e4737
Rey Victor Mendillo <59197211+rvmendillo@users.noreply.github.com>
2026-09-13 12:42:48 +0800

    Add NEXUS app entry point

---
+++ NexusApp/NEXUSApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct NEXUSApp: App {
+    @StateObject private var model = NexusModel()
+    var body: some Scene {
+        WindowGroup {
+            RootView().environmentObject(model)
+        }
+    }
+}
