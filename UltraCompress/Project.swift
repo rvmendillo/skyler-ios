6ddb7383f8032054f827faa9b2446684712c52c9
Rey Victor Mendillo <59197211+rvmendillo@users.noreply.github.com>
2026-09-13 16:38:42 +0800

    Add UltraCompress iOS project

---
+++ UltraCompress/Project.swift
@@ -0,0 +1,35 @@
+import ProjectDescription
+
+let project = Project(
+    name: "UltraCompress",
+    organizationName: "rvmendillo",
+    packages: [
+        .remote(url: "https://github.com/OlehKulykov/PLzmaSDK.git", requirement: .branch("master")),
+        .remote(url: "https://github.com/tomasf/Zip.git", requirement: .branch("main"))
+    ],
+    targets: [
+        .target(
+            name: "UltraCompress",
+            destinations: [.iPhone, .iPad],
+            product: .app,
+            bundleId: "com.rvmendillo.ultracompress",
+            deploymentTargets: .iOS("27.0"),
+            infoPlist: .extendingDefault(with: [
+                "CFBundleDisplayName": "UltraCompress",
+                "UILaunchScreen": [:],
+                "LSSupportsOpeningDocumentsInPlace": true,
+                "UIFileSharingEnabled": true
+            ]),
+            sources: ["Sources/**"],
+            dependencies: [
+                .package(product: "PLzmaSDK"),
+                .package(product: "Zip")
+            ],
+            settings: .settings(base: [
+                "SWIFT_VERSION": "6.0",
+                "TARGETED_DEVICE_FAMILY": "1,2",
+                "CODE_SIGN_STYLE": "Automatic"
+            ])
+        )
+    ]
+)
